e1696bcd004644ecebe2b22a96f753509ad71773
Teymur <m.teymur2003@gmail.com>
2023-07-31 10:06:36 +0400

    Tima

---
+++ main.js
@@ -0,0 +1,72 @@
+                    // 1
+
+// var student = {
+//     name: "David Rayy",
+//     sclass: "VI",
+//     rollno: 12
+//  };
+//   var propertyList = Object.keys(student).join(',');
+//  console.log(propertyList);
+ 
+
+
+                // 2
+// var library = [
+//     {
+//         title: 'The Road Ahead',
+//         author: 'Bill Gates',
+//         libraryID: 1254
+//     },
+//     {
+//         title: 'Walter Isaacson',
+//         author: 'Steve Jobs',
+//         libraryID: 4264
+//     },
+//     {
+//         title: 'Mockingjay: The Final Book of The Hunger Games',
+//         author: 'Suzanne Collins',
+//         libraryID: 3245
+//     }
+// ];
+// library.sort((a, b) => a.author.localeCompare(b.author));
+
+// console.log(library);
+
+
+
+
+                    //  3
+// function hasProperty(obj, property) {
+//     return property in obj;
+// }
+
+
+// var student = {
+//     name: "Tima",
+//     age: 20,
+//     rollno: 12345
+// };
+
+// console.log(hasProperty(student, "name")); 
+// console.log(hasProperty(student, "grade")); 
+
+
+
+  
+                    //    4
+// function getSubsets(str) {
+//     var subsets = [];
+//     for (var i = 0; i < str.length; i++) {
+//         for (var j = i + 1; j <= str.length; j++) {
+//             subsets.push(str.slice(i, j));
+//         }
+//     }
+//     return subsets;
+// }
+
+// var sampleString = "dog";
+// var result = getSubsets(sampleString);
+// console.log(result);
+
+
+
